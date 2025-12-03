3a83167d5aecec32301dc39a72cad0b2a6920d14
Moamen-Yazouri <moaamenalyazouro@gmail.com>
2025-12-03 15:11:27 +0200

    feat(auth): implement server actions for auth and add toast notifications
    refactor: move object utils to root utils directory
    chore: add sonner package for toast notifications

---
+++ modules/auth/auth.actions.ts
@@ -0,0 +1,25 @@
+
+"use server";
+
+import authService from "@/modules/auth/auth.service";
+import { TCreateUser } from "@/modules/user/user.types";
+
+
+export const signInAction = async (data: Pick<TCreateUser, "email" | "password">) => {
+    const { token, user } = await authService.signIn(data);
+
+    return {
+      success: true as const,
+      token,
+      data: user,
+    };
+  }
+
+export const signUpAction = async (data: TCreateUser) => {
+  const user = await authService.signUp(data);
+
+  return {
+    success: true as const,
+    data: user,
+  };
+}
