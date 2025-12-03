a394e2f72cab1d4214bf0bedb2b3bc591239cd75
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-03 21:26:53 +0200

    feat(category): add findCategoryById function to retrieve category by ID

---
+++ modules/category/category.repo.ts
@@ -0,0 +1,9 @@
+import prisma from "@/lib/prisma";
+
+export const findCategoryById = async (id: string) => {
+  return await prisma.category.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
+}
