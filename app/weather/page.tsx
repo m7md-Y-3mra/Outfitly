1c3554bf3ba0aa3cdc0de4b1225e3e824e21deb5
monazak <monazakout67@gmail.com>
2025-12-09 13:31:01 +0200

    feat: render WeatherPage in App component

---
+++ app/weather/page.tsx
@@ -0,0 +1,7 @@
+import WeatherPage from "@/modules/weather"; // Default import (no curly braces)
+
+const Weather = () => {  // You can rename this to Weather if preferred
+  return <WeatherPage />;
+};
+
+export default Weather;  // Or export default Weather if renaming the component
