fba5f73175180694924dbb5a5f12d1eac3a0e6c2
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-18 22:03:08 +0200

    fix: solve the loading structure

--- components/loading/loading.tsx
+++ components/loading/loading.tsx
@@ -3,7 +3,7 @@ import "./loading.css";
 export default function Loading() {
   return (
     <div className="sk-folding-cube">
-      .<div className="sk-cube1 sk-cube"></div>
+      <div className="sk-cube1 sk-cube"></div>
       <div className="sk-cube2 sk-cube"></div>
       <div className="sk-cube4 sk-cube"></div>
       <div className="sk-cube3 sk-cube"></div>
